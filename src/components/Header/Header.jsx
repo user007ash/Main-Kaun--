5d709c30e8f7e7f4153b974003111c99fab5ac5e
user007ash <ashu098p@gmail.com>
2025-04-09 17:18:07 +0530

    improve ui

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { Card } from "../ui/card";
 import { Link } from "react-router-dom";
-  
 
 const Header = () => {
   return (
-    <header className="container mx-auto my-6">
+    <header className="container mx-auto my-6 w-full">
       <Card className="p-4 shadow-md">
         <div className="flex justify-center items-center">
-          <Link to ="/" className="text-3xl font-bold tracking-wide text-blue-600 rounded-2xl border-2 border-blue-700 px-2 py-1">MK?</Link>
+          <Link
+            to="/"
+            className="text-3xl font-bold tracking-wide text-blue-600 rounded-2xl border-2 border-blue-700 px-2 py-1"
+          >
+            MK?
+          </Link>
         </div>
       </Card>
     </header>
@@ -16,4 +20,3 @@ const Header = () => {
 };
 
 export default Header;
-  
