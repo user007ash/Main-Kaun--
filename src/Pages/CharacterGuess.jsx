938328ee88424908e674dc3c575e823f63217754
user007ash <ashu098p@gmail.com>
2025-04-09 17:29:30 +0530

    Updated Guess Quiz Page

--- src/Pages/CharacterGuess.jsx
+++ src/Pages/CharacterGuess.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
-import { useParams} from 'react-router-dom'
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useParams } from "react-router-dom";
+import { cn } from "@/lib/utils";
+import { User } from "lucide-react";
+
+
 export const CharacterGuess = () => {
-  const { guess} = useParams();
+  const { genre } = useParams();
+
   return (
-    <div>CharacterGuess</div>
-  )
-}
+    <Card className="rounded-3xl flex-col flex-grow justify-center items-center relative flex bg-blue-300 dark:bg-black">
+      {/* Grid background */}
+      <div
+        className={cn(
+          "absolute inset-0",
+          "[background-size:40px_40px]",
+          "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+          "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]"
+        )}
+      />
+
+      {/* Radial fade mask */}
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black" />
+
+      {/* Main content */}
+      <Card className="z-10 p-4 capitalize text-4xl">
+        <CardHeader>
+          {genre}
+        </CardHeader>
+        <CardContent className="text-center">
+          <User/>
+        </CardContent>
+      </Card>
+    </Card>
+  );
+};
